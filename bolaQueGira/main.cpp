21ae3fe82077b8c3a2dda0c52b667a1e3ab6112f
dedpotato <aaronwhacker@gmail.com>
2021-07-31 01:21:41 -0400

    cambio bolas
    ahora es una matriz 2d

--- bolaQueGira/main.cpp
+++ bolaQueGira/main.cpp
@@ -5,12 +5,12 @@
 
 //codigo para crear circulo
 void
-crearCirculo(float *lista, float r, int num_segments){
+crearCirculo(float lista[][2], float r, int num_segments){
     for(int ii = 0; ii < num_segments; ii++){
             float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
 
-            lista[ii*2] = r * cosf(theta);//calculate the x component
-            lista[(ii*2)+1] = r * sinf(theta);//calculate the y component
+            lista[ii][0] = r * cosf(theta);//calculate the x component
+            lista[ii][1] = r * sinf(theta);//calculate the y component
         }
 }
 
@@ -23,16 +23,16 @@ dibujar(){
     glColor3f(1.0f,0.5f,0.0f);
 
     //segmentos x e y del circulo y el radio
-    float circulo[segmentos*2];
+    float circuloA[segmentos][2];
     float radio = 0.05;
 
     //crea una circulo basado en varias proporciones
-    crearCirculo(circulo,radio,segmentos);
+    crearCirculo(circuloA,radio,segmentos);
     //glTranslatef(0,0,0);
 
     glBegin(GL_POLYGON);
     for(int i = 0;i <=segmentos;i++){
-        glVertex2f(circulo[i*2], circulo[(i*2)+1]);
+        glVertex2f(circuloA[i][0], circuloA[i][1]);
     }
     glEnd();
 
