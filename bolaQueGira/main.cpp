202c9ec851071b52a31371b804fb6aad1df8d018
dedpotato <aaronwhacker@gmail.com>
2021-07-31 11:40:05 -0400

    cambio bola
    la figura de le bola se obtiene en main

--- bolaQueGira/main.cpp
+++ bolaQueGira/main.cpp
@@ -3,7 +3,14 @@
 #include <iostream>
 #include <math.h>
 
-//codigo para crear circulo
+const int segmentos = 16;
+const float radio = 0.05;
+//segmentos x e y del circulo y el radio
+float circuloA[segmentos][2];
+
+
+//recibe una matriz, radio, y segmentos, y regresa las corrdenadas del los segmentos de un  circulo
+//[0]=x [1]=y
 void
 crearCirculo(float lista[][2], float r, int num_segments){
     for(int ii = 0; ii < num_segments; ii++){
@@ -16,25 +23,17 @@ crearCirculo(float lista[][2], float r, int num_segments){
 
 void
 dibujar(){
-    int segmentos = 16;
-
     glClear(GL_COLOR_BUFFER_BIT);
     glPushMatrix();
     glColor3f(1.0f,0.5f,0.0f);
 
-    //segmentos x e y del circulo y el radio
-    float circuloA[segmentos][2];
-    float radio = 0.05;
-
-    //crea una circulo basado en varias proporciones
-    crearCirculo(circuloA,radio,segmentos);
     //glTranslatef(0,0,0);
 
+    //dibuja el circulo
     glBegin(GL_POLYGON);
     for(int i = 0;i <=segmentos;i++){
         glVertex2f(circuloA[i][0], circuloA[i][1]);
-    }
-    glEnd();
+    }glEnd();
 
     glPopMatrix();
 
@@ -59,6 +58,9 @@ actualizacion(){
 
 int
 main(int argc, char * args[]){
+
+    //crea una circulo basado en varias proporciones
+    crearCirculo(circuloA,radio,segmentos);
 
     glutInit(&argc, args);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
